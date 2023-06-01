ed063c8ba32011278e82e60f8f22d9e3cecfab81
詹焕超 <huanchao_zhan@feidee.com>
2023-06-01 10:30:47 +0800

    fix: 优化部分代码

--- src/hooks/useDraw.ts
+++ src/hooks/useDraw.ts
@@ -6,7 +6,7 @@ import Position from "../common/position"
 import Tree from "../common/tree"
 import { DRAW_CALLBACK_TYPE, ExtraOption } from '../common/draw/type'
 import Node, { ImageData, getChildNodeData, getInitData } from '../common/node/node'
-import { lineList, operateList, operateType, styleType } from '../constant/operate'
+import { lineList, operateList, operateType } from '../constant/operate'
 import EditTopic from '../common/operate/editTopic'
 import { changeIconDisabled, changeLineType, changeSnapshotDisabled, forTreeEvent, getLocalStorage } from '../utils/common'
 import { dataKey, optionKey } from '../constant'
@@ -21,7 +21,6 @@ import AddCommand from '../common/command/addCommand'
 import DelCommand from '../common/command/delCommand'
 import ImgCommand from '../common/command/imgCommand'
 import EditCommand from '../common/command/editCommand'
-import LineCommand from '../common/command/styleCommand'
 import StyleCommand from '../common/command/styleCommand'
 
 interface dataOption {
@@ -49,12 +48,7 @@ export default function () {
    * @param options 
    */
   function initPaper (options: ExtraOption) {
-    const nodeData = getLocalStorage(dataKey)
     data.tree = new Tree({data: getInitData()});
-    // 如果缓存中有节点数据
-    if (nodeData) {
-      data.tree.setRoot(arrayToTree(JSON.parse(nodeData))[0])
-    }
     initSetData()
     data.paper = new Paper('#paper');
     data.drawGenerator = new DrawGenerator(data.paper.getPaper());
@@ -99,52 +93,52 @@ export default function () {
   const getOperateFunc = (type: operateType, extraVal: number) => {
     const checkNodes = data.drawRender?.checkNodeList as Node[]
     const checkNode = checkNodes[0]
-    const func = [
-      () => { // 添加同级节点
+    const objOperate = {
+      [operateType.addTopic]: () => { // 添加同级节点
         const newNode = data.tree?.createNode(getChildNodeData(type, checkNode)) as Node;
         const addCommand = new AddCommand(newNode, checkNode, type)
         addCommand.execute()
         data.command?.pushCommand(addCommand)
         reDraw([newNode.id])
       },
-      () => { // 添加子级节点
+      [operateType.addSubTopic]: () => { // 添加子级节点
         const newNode = data.tree?.createNode(getChildNodeData(type, checkNode)) as Node;
         const addCommand = new AddCommand(newNode, checkNode, type)
         addCommand.execute()
         data.command?.pushCommand(addCommand)
         reDraw([newNode.id])
       },
-      () => { // 编辑操作
+      [operateType.editTopic]: () => { // 编辑操作
         const editCommand = new EditCommand(checkNode, editTopic as EditTopic)
         editCommand.execute()
         data.command?.pushCommand(editCommand)
       },
-      async () => { // 图片操作
+      [operateType.operateImg]: async () => { // 图片操作
         const imgCommand = new ImgCommand(checkNode, extraVal)
         await imgCommand.execute()
         data.command?.pushCommand(imgCommand)
         reDraw([])
       },
-      () => { // 删除节点
+      [operateType.delTopic]: () => { // 删除节点
         const delCommand = new DelCommand(checkNodes)
         delCommand.execute()
         data.command?.pushCommand(delCommand)
         reDraw(checkNodes.map(item => item.id))
       },
-      () => saveData(), // 保存数据
-      () => { // 撤销
+      [operateType.saveData]: () => saveData(), // 保存数据
+      [operateType.execute]: () => {
         const curCommand = data.command?.getExectedCommands()
         data.command?.pushUndoCommand(curCommand)
         curCommand.execute()
         reDraw([curCommand?.node?.id] || [])
       },
-      () => { // 恢复
+      [operateType.undo]: () => {
         const curCommand = data.command?.popCommand()
         curCommand.undo()
         reDraw(curCommand.lastCheckNodeIds || [])
       }
-    ]
-    return func
+    }
+    return objOperate[type]
   }
 
   /**
@@ -153,9 +147,10 @@ export default function () {
    * @param extraVal 额外的值 
    */
   async function handleOperateFunc (type: operateType, extraVal: number) {
-    await getOperateFunc(type, extraVal)[type]()
+    await getOperateFunc(type, extraVal)()
     changeSnapshotDisabled(data.command as CommandManager, operateList)
   }
+
   /**
    * 编辑框失焦事件
    * @param e 
@@ -163,6 +158,7 @@ export default function () {
   function handleEditBlur (e: Event) {
     editTopic && (editTopic as EditTopic).addEventBlur(e, data.drawRender?.data.checkNodeList[0] as Node, () => reDraw())
   }
+
   // 保存数据
   async function saveData () {
     showLoading({ text: '保存数据中...' })
@@ -201,8 +197,13 @@ export default function () {
 
   // 初始化设置缓存配置信息
   function initSetData () {
+    const nodeData = getLocalStorage(dataKey)
     const optionData = getLocalStorage(optionKey)
-    // 设置保存的值
+    // 如果缓存中有节点数据
+    if (nodeData) {
+      data.tree?.setRoot(arrayToTree(JSON.parse(nodeData))[0])
+    }
+    // 如果有配置信息的数据
     if (optionData) {
       const options = JSON.parse(optionData)
       // 线条类型
@@ -216,12 +217,14 @@ export default function () {
           NodeInfo[level] = options.nodeInfo[level]
         })
       }
+      // 线条颜色
       if (options.LINE_COLOR) {
         LINE_COLOR.value = options.LINE_COLOR
       }
     }
   }
 
+  // 样式配置
   function handleCommand (style: {value: number, type: number, key: string}) {
     const lineCommand = new StyleCommand(style)
     lineCommand.execute()
